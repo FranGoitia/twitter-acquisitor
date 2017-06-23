9601c8ca2f76a28fb0a96bc1ee4f5a1360e512a0
Francisco Goitia <[frangoitia@gmail.com]>
2017-06-23 10:19:33 -0300

    ready

---
+++ acquisitor.py
@@ -0,0 +1,138 @@
+import time
+from contextlib import suppress
+from tweepy import API, AppAuthHandler, Cursor, RateLimitError
+from geonamescache import GeonamesCache
+
+from model import (User, Follower, Keyword, Tweet, Search, Country, City,
+                   create_session, get_or_create)
+from keys import CONSUMER_KEY, CONSUMER_SECRET, TWITTER_HANDLE
+
+
+class Geo:
+
+    def __init__(self, session):
+        self.session = session
+        self.geonames = GeonamesCache()
+        self.countries = set([c['name'] for c in self.geonames.get_countries().values()])
+        self.cities = set([c['name'] for c in self.geonames.get_cities().values()])
+
+        # map cities names to country. when collisions, it maps the biggest city in the world
+        self.cities_to_countries = {}
+        codenames = {code: country['name'] for code, country in self.geonames.get_countries().items()}
+        cities_pop = {}
+        for c in self.geonames.get_cities().values():
+            country = self.cities_to_countries.get(c['name'])
+            if not country or c['population'] > cities_pop[c['name']]:
+                cities_pop[c['name']] = c['population']
+                self.cities_to_countries[c['name']] = codenames[c['countrycode']]
+
+    def get_place(self, text_loc):
+        """
+        gets most likely place in the world from text_loc
+        """
+        city, country = None, None
+        for w in text_loc.replace('/', ', ').replace('&', ', ').strip().split(', '):
+            if w in self.cities:
+                city = w
+            if w in self.countries:
+                country = w
+        country = self.cities_to_countries[city] if (city and not country) else country
+        if country:
+            country = get_or_create(self.session, Country, name=country).id
+        city = get_or_create(self.session, City, name=city, country_id=country)
+        return city
+
+
+class Acquisitor:
+
+    def __init__(self, consumer_key, consumer_secret, handle, session):
+        auth = AppAuthHandler(consumer_key, consumer_secret)
+        self.api = API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+        self.geo = Geo(session)
+        self.handle = handle
+        self.session = session
+
+    def register_followers(self, user_handle):
+        """
+        registers every follower of given User
+        """
+        user = self.api.get_user(user_handle)
+        followed = self._get_or_create_user(user, user.__dict__.get('status'))
+        for follower in Cursor(self.api.followers, id=user_handle).items():
+            f = self._get_or_create_user(follower, follower.__dict__.get('status'))
+            relationship = Follower(follower=f, followed=followed)
+            self.session.add(relationship)
+            self.session.commit()
+
+    def _get_or_create_user(self, user, status):
+        u = self.session.query(User).filter_by(handle=user.screen_name).first()
+        if not u:
+            city = None
+            if len(user.location.strip()) != 0:
+                city = self.geo.get_place(user.location)
+
+            u = User(user.id, user.screen_name, user.name, user.description,
+                     user.created_at, status, user.followers_count,
+                     user.friends_count, user.favourites_count,
+                     user.statuses_count, user.lang, city)
+            self.session.add(u)
+            self.session.flush()
+        return u
+
+    def register_the_search(self, phrase, language):
+        """
+        register tweets associated with phrase. only gets tweets a week older
+        """
+        since_tweet = None
+        max_id = None
+        tweets_n = 0
+        keyword = get_or_create(self.session, Keyword, text=phrase)
+        while tweets_n < 10000000:
+            kwargs = {'q': phrase, 'count': 100, 'lang': language}
+            if max_id:
+                kwargs['max_id'] = str(max_id - 1)
+            if since_tweet:
+                kwargs['since_id'] = since_tweet
+            new_tweets = self.api.search(**kwargs)
+            if not new_tweets:
+                print("No more tweets found")
+                break
+
+            for tweet in new_tweets:
+                t = self._create_tweet(tweet, keyword, language)
+                search = Search(keyword=keyword, tweet=t, lang=language)
+                self.session.add(search)
+                self.session.commit()
+            tweets_n += len(new_tweets)
+            max_id = new_tweets[-1].id
+
+    def _create_tweet(self, tweet, keyword, language):
+        author = self.session.query(User).filter_by(twitter_id=tweet.user.id).first()
+        if not author:
+            author = self._get_or_create_user(tweet.user, tweet)
+        t = Tweet(author=author, created_at=tweet.created_at, favourites_n=tweet.favorite_count,
+                  retweets_n=tweet.retweet_count, text=tweet.text, tweet_id=tweet.id,
+                  reply=tweet.in_reply_to_status_id is not None)
+        self.session.add(t)
+        self.session.flush()
+        return t
+
+    def follow(self, ids):
+        """
+        follows every user in ids
+        """
+        for user_id in ids:
+            self.api.create_friendship(user_id)
+
+    def unfollow(self, ids):
+        """
+        unfollow every user in ids
+        """
+        for user_id in ids:
+            self.api.destroy_friendship(user_id)
+
+
+if __name__ == '__main__':
+    acquisitor = Acquisitor(CONSUMER_KEY, CONSUMER_SECRET, TWITTER_HANDLE, create_session())
+    acquisitor.register_followers('FraudeMundial14')
+    # acquisitor.register_the_search('#grandt', 'es')
---
+++ model.py
@@ -0,0 +1,149 @@
+from datetime import datetime
+from sqlalchemy import (create_engine, Column, Integer, String, ForeignKey, Date,
+                        Boolean, DateTime, UniqueConstraint, PrimaryKeyConstraint)
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+DB_URL = 'sqlite:///antoine.db'
+
+
+def create_session():
+    Engine = create_engine(DB_URL, echo=False)
+    SessionMaker = sessionmaker(bind=Engine, autoflush=False)
+    Session = scoped_session(SessionMaker)
+    return Session
+
+
+def create_db():
+    Engine = create_engine(DB_URL, echo=False)
+    Base.metadata.create_all(Engine, checkfirst=True)
+
+
+def get_or_create(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        session.add(instance)
+        session.flush()
+        return instance
+
+
+class Country(Base):
+    __tablename__ = 'countries'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+
+    def __repr__(self):
+        return 'Country({0})'.format(self.name)
+
+
+class City(Base):
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    country_id = Column(ForeignKey('countries.id'), index=True)
+    country = relationship('Country', backref='cities')
+
+    UniqueConstraint(name, country_id)
+
+    def __repr__(self):
+        return 'City({0}, {1})'.format(self.name, self.country.name)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    twitter_id = Column(Integer, unique=True)
+    handle = Column(String)
+    name = Column(String)
+    description = Column(String)
+    city_id = Column(ForeignKey('cities.id'), index=True)
+    city = relationship('City', backref='users')
+    created_at = Column(Date)
+    days_since_tweet = Column(Integer)
+    tweets_n = Column(Integer)
+    favourites_n = Column(Integer)
+    followers_n = Column(Integer)
+    following_n = Column(Integer)
+
+    def __init__(self, twitter_id, handle, name, description, created_at, last_tweet,
+                 followers_n, following_n, favourites_n, tweets_n, lang, city):
+        self.city = city
+        self.twitter_id = twitter_id
+        self.handle = handle
+        self.name = name
+        self.description = description
+        self.created_at = created_at
+        if last_tweet:
+            self.days_since_tweet = abs(datetime.today() - last_tweet.created_at).days
+        self.followers_n = followers_n
+        self.following_n = following_n
+        self.favourites_n = favourites_n
+        self.tweets_n = tweets_n
+
+    def __repr__(self):
+        return 'User({0}, {1}, {2}, {3})'.format(self.twitter_id, self.handle, self.name, self.created_at)
+
+
+class Follower(Base):
+    __tablename__ = 'followers'
+
+    follower_id = Column(ForeignKey('users.id'), index=True)
+    follower = relationship('User', foreign_keys=[follower_id], backref='following')
+    followed_id = Column(ForeignKey('users.id'), index=True)
+    followed = relationship('User', foreign_keys=[followed_id], backref='followers')
+    datetime = Column(DateTime)
+
+    PrimaryKeyConstraint(follower_id, followed_id)
+
+    def __repr__(self):
+        return 'Follower({0}, {1})'.format(self.follower_id, self.followed_id)
+
+
+class Keyword(Base):
+    __tablename__ = 'keywords'
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String, unique=True)
+
+    def __repr__(self):
+        return 'Keyword({0})'.format(self.text)
+
+
+class Tweet(Base):
+    __tablename__ = 'tweets'
+
+    id = Column(Integer, primary_key=True)
+    author_id = Column(ForeignKey('users.id'), index=True)
+    author = relationship('User', foreign_keys=[author_id])
+    created_at = Column(DateTime)
+    favourites_n = Column(Integer)
+    retweets_n = Column(Integer)
+    text = Column(String)
+    reply = Column(Boolean)
+    tweet_id = Column(Integer, unique=True)
+
+    def __repr__(self):
+        return 'Tweet({0}, {1}, {2})'.format(self.text, self.author.name, self.created_at)
+
+
+class Search(Base):
+    __tablename__ = 'searches'
+
+    keyword_id = Column(ForeignKey('keywords.id'), index=True)
+    keyword = relationship('Keyword', backref='searches')
+    tweet_id = Column(ForeignKey('tweets.id'), index=True)
+    tweet = relationship('Tweet')
+    lang = Column(String)
+
+    PrimaryKeyConstraint(keyword_id, tweet_id)
+
+    def __repr__(self):
+        return 'Search({0}, {1})'.format(self.keyword.text, self.tweet.text)
